33c484df6f2ef7bb08926584ee5737564e589070
Victoria Ceci <145465524+vceci@users.noreply.github.com>
2023-11-02 14:53:22 +0000

    feat: create a menu option to list all movie names
    Creates a new menu option 'list' that displays the title of all movies in the movie file, excluding the cast. The program uses a for loop to iterate through the movies list and takes only the name field from each dictionary to display to the user.
    Moves code from display_menu.py to query.py.
    
    issue #3

--- query.py
+++ query.py
@@ -1,3 +1,20 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+def list():
+    for item in movies._movies:
+        print(item['name'])
+
+ans=True
+while ans:
+    print("q: quit \nlist: print all the movie names")
+    ans=input()
+    if ans=='q':
+        ans = None
+    elif ans=='list':
+        list()
+    else:
+        ans=True
+
+    
